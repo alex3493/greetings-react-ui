80195ca1a87cb8c5917702f6a19910300a716d08
Alex <alex.polonski@gmail.com>
2024-07-11 08:55:24 +0200

    Temporarily disable strict mode.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './App.scss'
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <App />
 )

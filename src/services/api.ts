611ec78e724496fae6de17976709dee4159d7c07
Alex <admin@smartcalc.org>
2024-07-29 23:53:19 +0200

    (fix) - api.ts: set default "Accept" request header.

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,6 +4,9 @@ import { setupInterceptors } from './interceptors'
 
 export const api = setupInterceptors(
   axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: process.env.REACT_APP_API_URL,
+    headers: {
+      Accept: 'application/json'
+    }
   })
 )

4dc733ecbabe4d8a1f8eefca16c03b151030192a
Alex <alex.polonski@gmail.com>
2024-08-09 08:09:48 +0200

    (feat) - Prepare busy indicator provider.

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,21 @@ import { AuthProvider } from './providers'
 import { Router } from './router'
 import Container from 'react-bootstrap/Container'
 import { ApiValidationProvider } from '@/providers/ApiValidationProvider'
+import { BusyIndicatorProvider } from '@/providers'
 
 function App() {
   return (
     <BrowserRouter>
-      <ApiValidationProvider>
-        <AuthProvider>
-          <NavBar />
-          <Container>
-            <Router />
-          </Container>
-        </AuthProvider>
-      </ApiValidationProvider>
+      <BusyIndicatorProvider>
+        <ApiValidationProvider>
+          <AuthProvider>
+            <NavBar />
+            <Container>
+              <Router />
+            </Container>
+          </AuthProvider>
+        </ApiValidationProvider>
+      </BusyIndicatorProvider>
     </BrowserRouter>
   )
 }
---
+++ src/contexts/BusyIndicatorContext/BusyIndicatorContext.ts
@@ -0,0 +1,22 @@
+import { createContext } from 'react'
+
+export type BusyEndpoint = {
+  url: string
+  type: 'sending' | 'receiving'
+  processingCount: number
+}
+
+export type BusyIndicatorData = {
+  busyEndpoints: BusyEndpoint[]
+  loadingCount: () => number
+  sendingCount: () => number
+  isEndpointBusy: (
+    endpoint: string,
+    activity?: 'all' | 'sending' | 'receiving'
+  ) => boolean
+  isBusy: (activity?: 'all' | 'sending' | 'receiving') => boolean
+}
+
+const BusyIndicatorContext = createContext({} as BusyIndicatorData)
+
+export default BusyIndicatorContext
